403390c1c0b7888274ef982a96fca8ac89cbf330
Артём <orten4eg@mail.ru>
2022-07-04 17:47:13 +0300

    Вроде сделал правильно, но как можно усовершенствовать эту тему, чтобы ещё лучше получилось...

--- Lesson_002/04_my_family.py
+++ Lesson_002/04_my_family.py
@@ -5,14 +5,20 @@
 
 # моя семья (минимум 3 элемента, есть еще дедушки и бабушки, если что)
 my_family = []
-
+my_family = ["Father", "Mother", "Sister", "Brother"]
 
 # список списков приблизителного роста членов вашей семьи
+Father = 180
+Mother_height = Mother = 170
+Sister_height = Sister = 160
+Brother_height = Brother = 150
 my_family_height = [
     # ['имя', рост],
-    [],
+    ["Father", Father], ["Mother",Mother_height], ["Sister", Sister_height], ["Brother", Brother_height]
 ]
 
+print("Father height - ", my_family_height[0][1], "cm")
+print("Family height = ", Father + Mother + Sister + Brother, "cm")
 # Выведите на консоль рост отца в формате
 #   Рост отца - ХХ см
 
