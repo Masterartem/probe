ea40249a87802c82aaeb644a10afb8ad81ac2b57
Артём <orten4eg@mail.ru>
2022-07-21 19:36:00 +0300

    Задание 4. Код норм читается?

--- Lesson_003/04_student.py
+++ Lesson_003/04_student.py
@@ -10,5 +10,13 @@
 #   Студенту надо попросить ХХХ.ХХ рублей
 
 educational_grant, expenses = 10000, 12000
+a = 1
+need_total_money = 0
+while a <= 10:
+    a += +1
+    month_inflation = expenses / 100 * 3
+    need_family_money = expenses - educational_grant
+    expenses = expenses + month_inflation
+    need_total_money += family_money
 
-# TODO здесь ваш код
+print(need_total_money)
