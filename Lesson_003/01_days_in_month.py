230292fb676597dacd9dbe8b544cf5666f711cc3
Артём <orten4eg@mail.ru>
2022-07-21 18:02:54 +0300

    Привет! Я вернулся =) Как по другому можно было решить это задание?

--- Lesson_003/01_days_in_month.py
+++ Lesson_003/01_days_in_month.py
@@ -11,3 +11,11 @@ user_input = input("Введите, пожалуйста, номер месяц�
 month = int(user_input)
 print('Вы ввели', month)
 
+if month == 1 or month == 3 or month == 5 or month == 7 or month == 8 or month == 10 or month == 12:
+    print("31")
+elif month == 4 or month == 6 or month == 9 or month == 11:
+    print("30")
+elif month == 2:
+    print("28")
+else:
+    print("Error")
