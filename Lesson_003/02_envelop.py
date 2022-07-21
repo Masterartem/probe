d622fc3efdb38c18b532d91d649ee4ced000f3a8
Артём <orten4eg@mail.ru>
2022-07-21 18:32:43 +0300

    Задание 2, со вторым не совсем понял как это сделать, тут походу матан нужно знать =))

--- Lesson_003/02_envelop.py
+++ Lesson_003/02_envelop.py
@@ -20,7 +20,11 @@ paper_x, paper_y = 8, 9
 # paper_x, paper_y = 9, 11
 # (просто раскоментировать нужную строку и проверить свой код)
 
-# TODO здесь ваш код
+if envelop_x >= paper_x and envelop_y >= paper_y:
+    print("YES")
+else:
+    print("NO")
+
 
 # Усложненное задание, решать по желанию.
 # Заданы размеры hole_x, hole_y прямоугольного отверстия и размеры brick_х, brick_у, brick_z кирпича (все размеры
@@ -28,15 +32,16 @@ paper_x, paper_y = 8, 9
 #
 # Определить, пройдет ли кирпич через отверстие (грани кирпича параллельны сторонам отверстия)
 
-# hole_x, hole_y = 8, 9
-# brick_x, brick_y, brick_z = 11, 10, 2
+hole_x, hole_y = 8, 9
+brick_x, brick_y, brick_z = 11, 10, 2
+
 # brick_x, brick_y, brick_z = 11, 2, 10
 # brick_x, brick_y, brick_z = 10, 11, 2
 # brick_x, brick_y, brick_z = 10, 2, 11
 # brick_x, brick_y, brick_z = 2, 10, 11
 # brick_x, brick_y, brick_z = 2, 11, 10
 # brick_x, brick_y, brick_z = 3, 5, 6
-# brick_x, brick_y, brick_z = 3, 6, 5
+# brick_x, brick_y, brick_z = 3, 6, 8
 # brick_x, brick_y, brick_z = 6, 3, 5
 # brick_x, brick_y, brick_z = 6, 5, 3
 # brick_x, brick_y, brick_z = 5, 6, 3
@@ -49,4 +54,10 @@ paper_x, paper_y = 8, 9
 # brick_x, brick_y, brick_z = 3, 11, 6
 # (просто раскоментировать нужную строку и проверить свой код)
 
-# TODO здесь ваш код
+hole_z = hole_x * hole_y / 10
+if hole_x >= brick_x and hole_y >= brick_y and hole_z >= brick_z:
+    print("YES")
+else:
+    print("No")
+
+
